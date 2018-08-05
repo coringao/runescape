02b9c3e6baf0da814e85a21193ab9fbc9047d5ed
Carlos Donizete Froes <coringao@riseup.net>
2018-08-05 18:35:10 -0300

    Updated script game

--- src/runescape.sh
+++ src/runescape.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#	Copyright (C) 2016-2017 Carlos Donizete Froes <coringao@riseup.net>
+#	Copyright (C) 2016-2018 Carlos Donizete Froes <coringao@riseup.net>
 #	Use of this script is governed by a BSD 2-clause license
 #	that can be found in the LICENSE file.
 #	Source code and contact info at https://gitlab.com/coringao/runescape
